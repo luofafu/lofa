e023270513f3952b3b50adc25a562b62db2cc5f6
lofa <3023723263@qq.com>
2026-03-16 14:59:21 +0800

    添加PY文件

---
+++ 学习笔记/2.数据类型.py
@@ -0,0 +1,14 @@
+# 整数 int
+print(type(520))
+# 浮点数 float
+print(type(2.2))
+# 字符串
+print(type('lofa'))
+# 类型转换
+a = '8.0'
+b = str(a)
+# c = int(a) 不能转换
+d = float(a)
+print(type(b))
+# print(type(c))
+print(type(d))
