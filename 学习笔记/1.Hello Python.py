d5fd8a9c1fbad01488a333a4d700ce469b884535
lofa <3023723263@qq.com>
2026-03-16 14:40:28 +0800

    添加PY文件

---
+++ 学习笔记/1.Hello Python.py
@@ -0,0 +1,11 @@
+# lofa
+
+print('Hello Python')
+# 分割符 sep=''
+print('Hello', 'Python', 'Lofa', sep='-')
+# 换行符 \n (默认)
+print('Hello', 'Python', 'Lofa', end='\n')
+# 变量 数字、字母、下划线，不以数字开头
+name = input('请输入你的名字：')
+print('你好{}'.format(name))
+
